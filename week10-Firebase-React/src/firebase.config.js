5ad31c57490852cb5fca102bc1e39dee0b33f5d7
noviannurrohman <noviannurrohman99@gmail.com>
2021-04-14 11:01:58 +0700

    week 10

---
+++ week10-Firebase-React/src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyBxvDt0UGrVXoHLdmYT8XRasjQMemVdloM",
+    authDomain: "praktikumweek10.firebaseapp.com",
+    projectId: "praktikumweek10",
+    storageBucket: "praktikumweek10.appspot.com",
+    messagingSenderId: "46263686010",
+    appId: "1:46263686010:web:b8452a4be9b31666433cc3",
+    measurementId: "G-BH58QPKN8L"  
+};
