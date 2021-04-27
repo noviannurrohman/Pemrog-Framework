85ab2ebb65f2df632dd3e939177e42ee262c9e80
noviannurrohman <noviannurrohman99@gmail.com>
2021-04-27 12:30:03 +0700

    week 12

---
+++ week12-Firebase-CRUD/src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDmIml-ZY17owcGSBNRzxkzFrDJBpjtpwo",
+  authDomain: "reactjs-12fe6.firebaseapp.com",
+  databaseURL: "https://reactjs-12fe6-default-rtdb.firebaseio.com",
+  projectId: "reactjs-12fe6",
+  storageBucket: "reactjs-12fe6.appspot.com",
+  messagingSenderId: "749673685143",
+  appId: "1:749673685143:web:cbb5a886678b79a292c9d2",
+  measurementId: "G-70CDJJF4EZ"
+};
+
+export default firebaseConfig;
